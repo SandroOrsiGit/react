654b1011bbbc1c1fbd8a8a54270dbb48d9ebeba2
Sandro Orsi <orsi.sandro.0@gmail.com>
2023-04-28 10:22:49 +0200

    merge

--- compositie/src/registerDialog.jsx
+++ compositie/src/registerDialog.jsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
 import Dialog from "./Dialog";
-export default function RegisterDialog() {
-	const [input, setInput] = useState("");
-	return (
-		<Dialog title="Register" subtitle="Registering is easy and fast" color="green">
-			<div>
-				Naam:
-				<input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
-				{input.length > 0 && <button>Registreer als {input} &raquo;</button>}
-			</div>
-		</Dialog>
-	);
+
+export default function WelcomeDialog() {
+    const [input, setInput] = useState('');
+
+    return (
+        <Dialog
+            title="Register"
+            subtitle="Registering is easy and fast."
+            color="blue"
+        >
+            <div>
+                Naam:
+                <input
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    text="text"
+                />
+                {input.length > 0 && <button>
+                    Register as {input} &raquo;
+                </button>}
+            </div>
+        </Dialog>
+    )
 }
